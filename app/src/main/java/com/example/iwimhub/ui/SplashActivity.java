7de1a55ea5ec32a7ffd1af26675d03ada6ef2b41
ioubnyacs <aubbenyas717@gmail.com>
2021-04-21 04:13:24 +0000

    splash & login layuot

--- app/src/main/java/com/example/iwimhub/ui/SplashActivity.java
+++ app/src/main/java/com/example/iwimhub/ui/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.example.iwimhub;
+package com.example.iwimhub.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.iwimhub.R;
 import com.example.iwimhub.ui.login.LoginActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +17,11 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(MainActivity.this , LoginActivity.class);
+                Intent i = new Intent(SplashActivity.this , LoginActivity.class);
                 startActivity(i);
                 finish();
             }
         }, 3000);
     }
 }
+
